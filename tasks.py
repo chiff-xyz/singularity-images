26b4dedce858201c445c78619bd31421ce89ef0b
Dennis Philippe Pereyra Jr <dppereyra@chiff.xyz>
2019-05-28 17:01:18 +0800

    Initial commit. Add build folder Add python build images Use python invoke for tasks

---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import Collection
+from invoke import run, task
+
+BUILD_CMD = 'sudo singularity build build/{app}-{os}.sif {app}/{os}.def'
+
+@task
+def clean(ctx, extra=''):
+    patterns = [
+        'build/*'
+    ]
+    if extra:
+        patterns.append(extra)
+    for pattern in patterns:
+        run('rm -Rf {}'.format(pattern))
+
+@task
+def build(ctx, app, os):
+    run(BUILD_CMD.format(app=app, os=os))
